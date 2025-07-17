389c2405b5ebdf33f15902e4c30446ee4134375a
Aleksandr Asanov <91asanov@gmail.com>
2025-07-17 15:54:04 +0300

    b1

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '89.169.181.45', 'yandextestserver.hopto.org']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '89.169.181.45', 'yandextestserver.hopto.org']
 
 
 # Application definition
